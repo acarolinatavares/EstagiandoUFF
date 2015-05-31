0e67b7a1d57e396e5e1d543d0895118229061070
Ana Carolina Tavares <acarolinatavaress@gmail.com>
2015-05-31 19:49:49 -0300

    por Ana Carolina: Enviando email de verificação de conta

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class UserMailer < ActionMailer::Base
-  default :from => "acarolinatavaress@gmail.com"
+  default :from => "estagiandouff@gmail.com"
 
   def registration_confirmation(usuario)
     @usuario = usuario
