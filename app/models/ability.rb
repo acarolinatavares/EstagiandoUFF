b426297b5d47bc9179af281f360a216f1adeaa6f
Ana Carolina Tavares <acarolinatavaress@gmail.com>
2015-06-05 20:23:01 -0300

    por Ana Carolina: Corrigindo ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,28 +1,33 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(usuario)
+  def initialize(user)
+    if user.nil?
+      can :create, Usuario
 
-    can :create, Usuario
-
-    if !usuario.nil? && usuario.administrador?
-      can :manage, :all
-    else
       can :read, :all
+    else
+      if user.administrador?
+        can :manage, :all
+      else
+        can :read, :all
 
-      can :autocomplete_empresas, Avaliacao
+        can :create, Empresa
+
+        can :autocomplete_empresas, Avaliacao
 
 
-      can :modify, Usuario do |user|
-        user == usuario
-      end
+        can :modify, Usuario do |user1|
+          user1 == user
+        end
 
-      can [:modify,:destroy], Avaliacao do |avaliacao|
-        avaliacao.usuario == usuario
-      end
+        can [:modify,:destroy], Avaliacao do |avaliacao|
+          avaliacao.usuario == user
+        end
 
-      can [:modify,:destroy], Comentario do |comentario|
-        comentario.usuario == usuario
+        can [:modify,:destroy], Comentario do |comentario|
+          comentario.usuario == user
+        end
       end
     end
 
