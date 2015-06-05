d86b59dde6fe8c4341a886bd47c7e71083d74d90
Ana Carolina Tavares <acarolinatavaress@gmail.com>
2015-06-04 23:00:21 -0300

    por Ana Carolina: Configurando ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,33 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
+  def initialize(usuario)
 
-    user ||= Usuario.new
-    if user.administrador
+    usuario ||= Usuario.new
+    if usuario.administrador?
       can :manage, :all
     else
       can :read, :all
+
+      can :create, Empresa
+
+      can :create, Usuario
+
+      can :modify, Usuario do |user|
+        user == usuario
+      end
+
+      can :create, Avaliacao
+
+      can [:modify,:destroy], Avaliacao do |avaliacao|
+        avaliacao.usuario == usuario
+      end
+
+      can :create, Comentario
+
+      can [:modify,:destroy], Comentario do |comentario|
+        comentario.usuario == usuario
+      end
     end
 
   end
