73263f5adf5256140b61cb32754155ad3d85d9df
Ana Carolina Tavares <acarolinatavaress@gmail.com>
2015-07-01 10:16:26 -0300

    por Ana Carolina: Limitando 1 avaliação por pessoa+empresa

--- app/controllers/avaliacoes_controller.rb
+++ app/controllers/avaliacoes_controller.rb
@@ -20,13 +20,18 @@ class AvaliacoesController < ApplicationController
   end
 
   def create
-    @avaliacao = Avaliacao.new(params[:avaliacao])
-    @avaliacao.usuario_id = current_user.id
-
-    if @avaliacao.save
-      redirect_to @avaliacao, :notice => 'Avaliação cadastrada com sucesso!'
+    avaliacao = Avaliacao.where(:empresa_id=>params[:avaliacao][:empresa_id], :usuario_id => current_user.id)
+    if avaliacao.any?
+      redirect_to avaliacao.first, :flash => { :error => "Você já avaliou essa empresa." }
     else
-      render new_avaliacao_path, :flash => { :error => "A avaliação não pode ser salva. Erros: #{@avaliacao.errors}." }
+      @avaliacao = Avaliacao.new(params[:avaliacao])
+      @avaliacao.usuario_id = current_user.id
+
+      if @avaliacao.save
+        redirect_to @avaliacao, :notice => 'Avaliação cadastrada com sucesso!'
+      else
+        render new_avaliacao_path, :flash => { :error => "A avaliação não pode ser salva. Erros: #{@avaliacao.errors}." }
+      end
     end
   end
 
@@ -34,11 +39,11 @@ class AvaliacoesController < ApplicationController
     @avaliacao = Avaliacao.find(params[:id])
     @avaliacao.usuario_id = current_user.id
 
-      if @avaliacao.update_attributes(params[:avaliacao])
-        redirect_to @avaliacao, :notice => 'Avaliação atualizada com sucesso!.'
-      else
-        render edit_avaliacao_path, :flash => { :error => "A avaliação não pode ser alterada. Erros: #{@avaliacao.errors}." }
-      end
+    if @avaliacao.update_attributes(params[:avaliacao])
+      redirect_to @avaliacao, :notice => 'Avaliação atualizada com sucesso!.'
+    else
+      render edit_avaliacao_path, :flash => { :error => "A avaliação não pode ser alterada. Erros: #{@avaliacao.errors}." }
+    end
   end
 
   def destroy
